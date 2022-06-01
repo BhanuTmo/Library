1c507493e350bf0014979d9be7cdbc08c6def198
Bhanu <bhanuprasad.gollapudi2@t-mobile.com>
2022-06-01 14:59:30 -0500

    changes in test file

--- test/app.test.js
+++ test/app.test.js
@@ -18,5 +18,40 @@ describe("Base routes", () => {
 });
 
 describe("Additional Routes", () => {
-
+  it("provides a response to the health endpoint", (done) => {
+    chai
+      .request(app)
+      .post("/api/books")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
 });
+
+
+describe("Additional Routes", () => {
+  it("provides a response to the health endpoint", (done) => {
+    chai
+      .request(app)
+      .get("/api/books")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+
+describe("Additional Routes", () => {
+  it("provides a response to the health endpoint", (done) => {
+    chai
+      .request(app)
+      .delete("/api/books")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
